6e216e534f32a0c17fc92f9e2805850bc4d700a0
Bernardo Dill <bernardodill10@gmail.com>
2017-04-13 19:32:11 -0300

    Tabela finalizada

--- script.js
+++ script.js
@@ -3,37 +3,32 @@
 	var nomeItem;
 	var quantidade=1;
 	var precoUnitario;
-
-
+	var numLinhas=0;
+	var total=0
 	
 	$(".campo-nome").on('input', function(){
 		nomeItem = $(".campo-nome").val();
 	});
 		
-
-	
 	$('.campo-quantidade').on('input', function(){
 		quantidade = $(".campo-quantidade").val();
-		
 	});
 
-	 
 	$('.campo-precoUnitario').mask('000.000,00', {reverse:true});
 	
 	$('.campo-precoUnitario').on('input', function(){
 		precoUnitario = $('.campo-precoUnitario').val();
 		//console.log(precoUnitario);
-		
 	});
 	
 	function pontoVirgula(){
 		precoUnitario = precoUnitario.replace('.','');
 		
 		precoUnitario = precoUnitario.replace(',','.');
-		
 	}
 
-	function iniciaLista(){
+
+	function ShowHideList(){
 		if($(".lista").hasClass("hidden")){
 			$(".lista").removeClass("hidden");
 			$(".lista").show();
@@ -43,8 +38,15 @@
 		}
 	}
 
-	function calculaSubTotal(){
+	function calculaSubTotal(){	
 		return quantidade * precoUnitario;
+	}
+
+	function formataNumero(valor){
+		if(valor.toString().indexOf(".") == -1){
+			return valor = parseFloat(valor).toFixed(2);
+		}
+		return valor;
 	}
 
 
@@ -53,29 +55,59 @@
 			"<tr>"+
 				"<td><input type='text' readonly class='input-lista' value='"+nomeItem+"'></td>"+
 				"<td><input type='text' readonly class='input-lista' value='"+quantidade+"'></td>"+
-				"<td><input type='text' readonly class='input-lista' value='"+precoUnitario+"'></td>"+
-				"<td><input type='text' readonly class='input-lista' value='"+calculaSubTotal().toFixed(2)+"'></td>"+
-				"<td><a href='javascript:void(0)'>Remover Linha</a></td>"+
+				"<td><input type='text' readonly class='input-lista' value='"+formataNumero(precoUnitario)+"'></td>"+
+				"<td><input type='text' readonly class='input-lista subTotal' value='"+formataNumero(calculaSubTotal()).toFixed(2)+"'></td>"+
+				"<td class='r'><a href='javascript:void(0)' class='remove'>Excluir</a></td>"+
 			"</tr>"
 		);
-			
-		
+		resetaCampos();
+		atualizaTotal();
 	}
 
+
+	function resetaCampos(){
+		$('.campo-nome').val("");
+		nomeItem="";
+		$(".campo-quantidade").val(1);
+		quantidade=1;
+		$(".campo-precoUnitario").val("");
+		precoUnitario=0;
+	}
+
+	$(document).on("click", ".remove", function(){
+		
+		$(this).parent().parent().remove();
+		numLinhas--;
+		if(numLinhas == 0){
+			ShowHideList();
+		}
+		atualizaTotal();
+	});
+	
 	$(".add-button").on("click", function(){
+		
 		if(nomeItem != "" && nomeItem != undefined  && quantidade>=1 && precoUnitario!="" && precoUnitario != undefined){
 			
 			if($("form.lista").hasClass('hidden')){
-				iniciaLista();	
+				ShowHideList();	
 			};
 			pontoVirgula();
 			adicionaItem();
+			numLinhas++;
 		}
 	});
 
+	function atualizaTotal(){
+		total=0;
+		$("table tbody .subTotal").each(function(){
+			total += parseFloat(this.value);
+
+		});
+		$(".valor-total").text("Total: R$ "+total.toFixed(2));
+	}
+
+
 	
-
-
 	
 
 
